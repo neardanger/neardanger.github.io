3bea13feb397fdcfad80bda8ef9e9d8936becb51
steven magadan <steven.magadan@hotmail.com>
2017-03-21 15:25:37 -0700

    removed script

--- js/script.js
+++ js/script.js
@@ -12,42 +12,4 @@
         
   });
 
-  function letterLarge(a){
-     var input = a.split('')[0]
-     var upper = a.toUpperCase()[0]
-     console.log(input)
-     console.log(upper)
-
-     if(input == upper){
-       console.log("This is an uppercase letter")
-     } else {
-       console.log("This is not an uppercase")
-     }
-  }
-
-  letterLarge("string")
-  
-
-
-  var arrays = [1,2,3,4,5,6,7]
-
-  function addUpArray(arrays){
-    var answer = 0
-    for(var i=0;i<arrays.length;i++){
-      answer += arrays[i]
-     
-    }
-     console.log(answer)
-  }
-
-  addUpArray(arrays)
-
-
-  function findAreaCircle(a){
-
-    var area = Math.PI * Math.pow(a)
-
-    return area
-  }
-
-  findAreaCircle(10)
+ 
