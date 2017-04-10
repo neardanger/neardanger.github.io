b8e0671e67a666d642575ef9db4e99c22c262505
steven magadan <steven.magadan@hotmail.com>
2017-04-10 13:49:03 -0700

    removed tooltips

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
     $('.scrollspy').scrollSpy();
 
       $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
+      menuWidth: 300, 
+      edge: 'left', 
+      closeOnClick: true, 
+      draggable: true
     }
   );
 
